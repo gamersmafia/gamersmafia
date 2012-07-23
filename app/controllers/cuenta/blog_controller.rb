ea9f9cf073c2829aaa56322bb5f6c390d64bde8f
Draco351 <leandroclpresti@gmail.com>
2012-07-23 15:53:18 +0200

    Refactorización a blog_controller.rb

--- app/controllers/cuenta/blog_controller.rb
+++ app/controllers/cuenta/blog_controller.rb
@@ -2,7 +2,9 @@
 class Cuenta::BlogController < ApplicationController
   before_filter :require_auth_users
 
-  #verify :method => :post, :only => [:update, :destroy], :redirect_to => '/cuenta/blog'
+  #verify :method => (:post,
+  #                   :only) => [:update,
+  #                              :destroy], :redirect_to => '/cuenta/blog'
 
   def index
     navpath2<< ['Preferencias', '/cuenta']
@@ -15,7 +17,8 @@ class Cuenta::BlogController < ApplicationController
   end
 
   def create
-    be = @user.blogentries.create(params[:blogentry].merge({ :state => Cms::PUBLISHED }))
+    be = @user.blogentries.create(params[:blogentry].merge(
+                                                  { :state => Cms::PUBLISHED }))
     if be
       Users.add_to_tracker(@user, be.unique_content)
       flash[:notice] = 'Entrada creada correctamente'
@@ -28,14 +31,19 @@ class Cuenta::BlogController < ApplicationController
 
   def edit
     navpath2<< ['Preferencias', '/cuenta']
-    @blogentry = Blogentry.find_or_404(:first, :conditions => ['id = ?', params[:id]])
-    raise AccessDenied unless @user.has_admin_permission?(:capo) || @user.id == @blogentry.user_id
+    @blogentry = Blogentry.find_or_404(:first,
+                                       :conditions => ['id = ?', params[:id]])
+    raise AccessDenied unless (@user.has_admin_permission?(:capo) ||
+                               @user.id == @blogentry.user_id)
     @title = "Editar entrada de blog \"#{@blogentry.title}\""
   end
 
   def update
     @blogentry = Blogentry.find(params[:id])
-    raise ActiveRecord::RecordNotFound unless (user_is_authed and Cms::user_can_edit_content?(@user, @blogentry))
+    raise ActiveRecord::RecordNotFound unless (user_is_authed and
+                                               Cms::user_can_edit_content?(
+                                                    @user,
+                                                    @blogentry))
 
     if @blogentry.update_attributes(params[:blogentry].pass_sym(:title, :main))
       flash[:notice] = 'Entrada actualizada correctamente'
@@ -48,7 +56,10 @@ class Cuenta::BlogController < ApplicationController
 
   def destroy
     @blogentry = Blogentry.find(params[:id])
-    raise ActiveRecord::RecordNotFound unless (user_is_authed and Cms::user_can_edit_content?(@user, @blogentry))
+    raise ActiveRecord::RecordNotFound unless (user_is_authed and
+                                               Cms::user_can_edit_content?(
+                                                    @user,
+                                                    @blogentry))
     @blogentry.change_state(Cms::DELETED, @user)
     flash[:notice] = 'Entrada borrada correctamente'
     redirect_to :action => 'index'
